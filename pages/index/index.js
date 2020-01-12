df6013417c67a1e3011a18d4a6f1349a0d132048
guang77 <guangy130@outlook.com>
2020-01-12 13:24:34 +0800

    1-1221

--- pages/index/index.js
+++ pages/index/index.js
@@ -21,7 +21,7 @@ Page({
     ],
  
 
-    listlive:[
+    listLive:[
       { id: 1, listavatar: "", listname: "主播一", listperson: "2.5万", listaddr: "北京", listdet: "这是于是宝石的直播间", livbgpic:"/img/recommand_pic1.png"}, 
       { id: 2, listavatar: "", listname: "主播二", listperson: "2.5万", listaddr: "昆明", listdet: "这是翡翠直播间", livbgpic:"/img/recommand_pic2.png"}
     ],
